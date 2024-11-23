d32da616e9df1b1188e5b72d03a53d3399910e62
mychellew <46471656+mychellew@users.noreply.github.com>
2024-11-23 10:28:51 -0500

    Updated relative paths that caused issues after removing cd data

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -36,27 +36,27 @@ ls -R ./data/raw
 #pwd
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-mkdir processed
-mkdir ./processed/server_logs ./processed/user_logs ./processed/event_logs
+mkdir ./data/processed
+mkdir ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 #pwd
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 #pwd
-cp ./raw/*server*.log ./processed/server_logs
+cp ./data/raw/*server*.log ./data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
 #pwd
-cp ./raw/*user*.log ./processed/user_logs
-cp ./raw/*event*.log ./processed/event_logs
+cp ./data/raw/*user*.log ./data/processed/user_logs
+cp ./data/raw/*event*.log ./data/processed/event_logs
 #pwd
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm ./raw/*ipaddr* 
-rm ./processed/user_logs/*ipaddr*
+rm ./data/raw/*ipaddr* 
+rm ./data/processed/user_logs/*ipaddr*
 #pwd
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-ls -R ./processed/ >> inventory.txt
+ls -R ./data/processed/ >> ./data/inventory.txt
 
 ###########################################
 
